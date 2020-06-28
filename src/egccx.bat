a0ca1207b206357330547098cd9a270aa5fe9380
TheDeadFish <michaelstamper1@gmail.com>
2020-06-28 10:05:03 +0000

    egccx.bat:CPU fixed trailing whitespace

--- src/egccx.bat
+++ src/egccx.bat
@@ -1,6 +1,8 @@
 @set EXM_TMP="%1"
 @if not %EXM_TMP:x64=%==%EXM_TMP% (
-	@SET CPU=x64 ) else ( @SET CPU=x86 )
+	@SET CPU=x64
+) else ( @SET CPU=x86
+)
 @call %~dp0\env_gcc\ccx_env.bat GCC_9.2
 ::@set CMAKE=%PROGRAMS%\progs\cmake-3.16.2-win64-x64\bin\cmake.exe -G "MinGW Makefiles"
 @set CMAKE=%PROGRAMS%\progs\cmake-3.16.2-win64-x64\bin\cmake.exe -G Ninja
